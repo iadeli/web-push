06aa1dfdfde943517f42bdfb4962a51c9a0f5b4a
i.adeli <29071988ia>
2025-05-10 15:40:57 +0330

    show pushdata

--- push.js
+++ push.js
@@ -9,4 +9,10 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:i.adeli.3487@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
+let sub = { 
+    "endpoint": "https://fcm.googleapis.com/fcm/send/eJG02w0h-RU:APA91bEnxU_4CHf9-J6Czt8fHISRoeEpZ0z33zklBCVpSeEdsFGT02_lCbyGZElEvDrE_6yTv8h2zwOMYjrDb0kI_F5lGI8qimBBMnF9FHknmH5JX464KWB19qZiMz-OhFUowFFqhIDJ", 
+    "expirationTime": null, 
+    "keys": { "p256dh": "BG5hVY28BJr7pnIh1-h5_FrRZ1w1osrR0TTOxDxQfimC9CpYZ5wUvR8e962uL9U6ED3tXzX_L4xY1oaWldrqUxc", "auth": "CFMAiki2OKYzGBieyNOjkQ" } 
+}
+
 push.sendNotification(sub, 'test message');
